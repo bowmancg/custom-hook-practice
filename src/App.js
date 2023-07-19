812d6140208e2200cfdbdd2c389afeeb75fbc640
Chandler Bowman <doctorgrant99@gmail.com>
2023-07-19 17:20:13 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import NewTask from './components/NewTask/NewTask';
+import Tasks from './components/Tasks/Tasks';
+import useHttp from './hooks/use-http';
+
 
 function App() {
+  const [tasks, setTasks] = useState([])
+
+  const transformTasks = tasksObj => {
+    const loadedTasks = []
+
+    for (const taskKey in tasksObj) {
+      loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text })
+    }
+
+    setTasks(loadedTasks)
+  }
+
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp({ url: 'https://http-practice-68dcb-default-rtdb.firebaseio.com/tasks.json' }, transformTasks)
+
+  useEffect(() => {
+    fetchTasks()
+  }, [])
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+    </React.Fragment>
   );
 }
 
